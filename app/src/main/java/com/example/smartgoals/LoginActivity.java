6892b11cd44c50f9c317344217a80bb8c10a8407
Daniel Nguyen <theonedanielnguyen@gmail.com>
2020-12-18 18:43:14 -0800

    Testing

---
+++ app/src/main/java/com/example/smartgoals/LoginActivity.java
@@ -0,0 +1,68 @@
+package com.example.smartgoals;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText email;
+    EditText password;
+    Button loginButton;
+    Button registerButton;
+
+    String correct_email = "daniel@gmail.com";
+    String correct_pass = "daniel";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.loginEmail);
+        password = findViewById(R.id.loginPassword);
+        loginButton = findViewById(R.id.loginButton);
+        registerButton = findViewById(R.id.loginRegister);
+
+        loginButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkDataEntered();
+            }
+        });
+
+        registerButton.setOnClickListener( new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginRedirect = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(loginRedirect);
+            }});
+
+    }
+
+
+
+    void checkDataEntered() {
+        if (TextUtils.isEmpty(email.getText().toString()) ||
+                TextUtils.isEmpty(password.getText().toString()) ||
+                !(email.getText().toString().equals(correct_email)) ||
+                !(password.getText().toString().equals(correct_pass))) {
+            Toast invalid = Toast.makeText(this, "Invalid Login!", Toast.LENGTH_SHORT);
+            email.setError("Invalid Credentials!");
+            password.setError("Invalid Credentials!");
+            invalid.show();
+        }
+        else {
+            Toast loginSuccess = Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT);
+            loginSuccess.show();
+        }
+
+
+    }
+}
