48de629ec80fc29610c3a4fd1b3c1270002ed80e
GBurroughs60 <63984081+GBurroughs60@users.noreply.github.com>
2020-12-21 18:10:03 -0500

    Goal Repo and Database addition

---
+++ app/src/main/java/com/example/smartgoals/databases/GoalDatabase.java
@@ -0,0 +1,55 @@
+package com.example.smartgoals.databases;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import androidx.annotation.NonNull;
+import androidx.room.Dao;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import com.example.smartgoals.daos.UserDao;
+import com.example.smartgoals.models.User;
+
+@Database(entities = {Goal.class}, version=1)
+public abstract class GoalDatabase extends RoomDatabase {
+    public static GoalDatabase goalDatabase;
+
+    public abstract GoalDao goalDao();
+
+    public static synchronized GoalDatabase getGoalDatabase(Context context) {
+        if (goalDatabase == null) {
+            goalDatabase = Room.databaseBuilder(context.getApplicationContext(), GoalDatabase.class, "goal_database")
+                    .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
+                    .build();
+        }
+        return goalDatabase;
+    }
+
+    // Fill up an example user when database is created
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new GoalDatabase.PopulateDbAsyncTask(goalDatabase).execute();
+        }
+    };
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
+        private GoalDao goalDao;
+
+        private PopulateDbAsyncTask(GoalDatabase db) {
+            goalDao = db.goalDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            goalDao.insert(new Goal());
+            return null;
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/smartgoals/repositories/GoalRepository.java
@@ -0,0 +1,91 @@
+package com.example.smartgoals.repositories;
+
+import android.app.Application;
+import android.os.AsyncTask;
+
+import com.example.smartgoals.daos.UserDao;
+import com.example.smartgoals.models.User;
+
+public class GoalRepository {
+
+    private GoalDao goalDao;
+    private Goal foundGoal;
+
+    public GoalRepository(Application application) {
+        GoalRepository goalRepository = GoalDatabase.getGoalDatabase(application);
+        goalDao = goalDatabase.goalDao();
+    }
+
+    public void insert(Goal goal) {
+        new GoalRepository.InsertGoalAsyncTask(goalDao).execute(goal);
+    }
+
+    public void update(Goal goal) {
+        new GoalRepository.UpdateGoalAsyncTask(goalDao).execute(goal);
+    }
+
+    public void delete(Goal goal) {
+        new GoalRepository.DeleteGoalAsyncTask(goalDao).execute(goal);
+    }
+
+    public void deleteAllGoals() { new GoalRepository.DeleteAllGoalsAsyncTask(goalDao).execute();
+    }
+
+    public Goal getGoalById() { return foundGoal; }
+
+    private static class InsertGoalAsyncTask extends AsyncTask<Goal, Void, Void> {
+        private GoalDao goalDao;
+
+        private InsertGoalAsyncTask(GoalDao goalDao) {
+            this.goalDao = goalDao;
+        }
+
+        @Override
+        protected Void doInBackground(Goal... users) {
+            goalDao.insert(goals[0]);
+            return null;
+        }
+    }
+
+    private static class UpdateGoalAsyncTask extends AsyncTask<Goal, Void, Void> {
+        private GoalDao goalDao;
+
+        private UpdateGoalAsyncTask(GoalDao goalDao) {
+            this.goalDao = goalDao;
+        }
+
+        @Override
+        protected Void doInBackground(Goal... goals) {
+            goalDao.update(goals[0]);
+            return null;
+        }
+    }
+
+    private static class DeleteGoalAsyncTask extends AsyncTask<Goal, Void, Void> {
+        private GoalDao goalDao;
+
+        private DeleteGoalAsyncTask(GoalDao goalDao) {
+            this.goalDao = goalDao;
+        }
+
+        @Override
+        protected Void doInBackground(Goal... goals) {
+            goalDao.delete(goals[0]);
+            return null;
+        }
+    }
+
+    private static class DeleteAllGoalsAsyncTask extends AsyncTask<Void, Void, Void> {
+        private GoalDao goalDao;
+
+        private DeleteAllGoalsAsyncTask(GoalDao goalDao) {
+            this.goalDao = goalDao;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            goalDao.deleteAllGoals();
+            return null;
+        }
+    }
+}
