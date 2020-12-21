bcabc34cbaa21a52752c8495948328d31cd827af
ZachSpriggs <60864026+ZachSpriggs@users.noreply.github.com>
2020-12-21 14:57:43 -0800

    Added goal and task models

---
+++ app/src/main/java/com/example/smartgoals/models/UserWithGoals.java
@@ -0,0 +1,20 @@
+package com.example.smartgoals.models;
+
+import androidx.room.Embedded;
+import androidx.room.Relation;
+import java.util.List;
+
+public class UserWithGoals {
+
+    @Embedded public User user;
+    @Relation(
+            parentColumn = "id",
+            entityColumn = "goal_id"
+    )
+    public List<Goal> goals;
+
+    public UserWithGoals(User user, List<Goal> goals){
+        this.user = user;
+        this.goals = goals;
+    }
+}
